921eed8e4ac6e9b4796a778a17e51237ccd68727
hanys619 <song619@gmail.com>
2023-04-26 10:48:04 +0900

    메인/대기실/팝업추가

--- mobile/js/common.js
+++ mobile/js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	inputTextFocus('.ipt_txt');
+	inputTextFocus('.inp_area');
 });
 
 /* 높이 */
@@ -15,14 +15,14 @@ var heightResize = function(){
 	$(window).resize();
 };
 
-/* input 텍스트 focus */
-var inputTextFocus = function(ipt){
-	var $ipt = $(ipt).find('input');
-	$ipt.focus(function(){
-		$(this).parent(ipt).addClass('ipt_focus');
+/* input text focus */
+var inputTextFocus = function(inp){
+	var $inp = $(inp).find('input');
+	$inp.focus(function(){
+		$(this).parent(inp).addClass('inp_focus');
 	});
-	$ipt.blur(function(){
-		$(this).parent(ipt).removeClass('ipt_focus');
+	$inp.blur(function(){
+		$(this).parent(inp).removeClass('inp_focus');
 	});
 };
 
@@ -58,6 +58,7 @@ $('.disable').click(function(){
 /* 채팅 화면 열기 */
 $('.set_controls .chat').click(function(e){
 	e.preventDefault();
+	$('.btn_toggle').trigger('click');	
 	$('.page_room').hide();
 	$('.page_chatting').show();
 	$('html, body').addClass('bg_white');
@@ -79,6 +80,7 @@ $('.page_chatting .btn_close').click(function(e){
 /* 참여자 화면 열기 */
 $('.set_controls .member').click(function(e){
 	e.preventDefault();
+	$('.btn_toggle').trigger('click');	
 	$('.page_room').hide();
 	$('.page_member').show();
 	$('html, body').addClass('bg_white').animate({scrollTop : 0}, 1);
@@ -96,13 +98,53 @@ $('.page_member .btn_close').click(function(e){
 /* 초대코드 복사 */
 $('.set_controls .codecopy').click(function(e){
 	e.preventDefault();
+	$('.btn_toggle').trigger('click');	
 	$('.pop_msg').fadeIn(400).delay(1000).fadeOut(400); //fade out after 4 seconds
 });
+
+/* 설정 화면 열기 */
+$('.set_controls .set').click(function(e){
+	e.preventDefault();
+	$('.btn_toggle').trigger('click');	
+	$('.page_room').hide();
+	$('.page_set').show();
+	$('html, body').addClass('bg_white').animate({scrollTop : 0}, 1);
+});
+
+/* 설정 화면 닫기 */
+$('.page_set .btn_close').click(function(e){
+	e.preventDefault();
+	$('.page_room').show();
+	$('.page_set').hide();
+	$(window).resize();
+	$('html, body').removeClass('bg_white');
+});
+
+$('.disable').click(function(){
+	preventDefault();
+});
+
+/* selectbox */
+$('select:not(.ignore)').niceSelect();
 
 /* 화면 선택 */
 $('.list_video li').click(function(e){
 	e.stopPropagation();
 	$(this).toggleClass('on').siblings().removeClass('on');
+});
+
+if ($('.modal').hasClass('on') === true) {
+	$('body').css('overflow', 'hidden');
+};
+
+/* 탭메뉴 */
+var tabNav = $('.tab_menu li');
+$(tabNav).click(function(){
+	var tabView = $(tabNav).index(this);
+	$('.tab_menu li').removeClass('on');
+	$(this).addClass('on');
+	$('.tab_cont>div').hide();
+	$('.tab_cont>div').eq(tabView).show();
 });
 
 /* 팝업 열기 */
@@ -118,44 +160,41 @@ var modalOpen = function(modal, posi){
 			modalPosi(modal, posi);
 		};
 	});
-	$modal.find('.modal_backdrop').click(function(){
+	/* $modal.find('.modal_backdrop').click(function(){
+		modalClose(modal);
+	}); */
+	$modal.find('.btn_close').click(function(){
 		modalClose(modal);
 	});
-	$modal.find('.btn_close').click(function(){
+	$modal.find('.btn_gray').click(function(){
 		modalClose(modal);
 	});
 };
 
 /* 팝업 위치 */
-var modalPosi = function(modal, posi){
+var modalPosi = function(modal, posi) {
 	var $modal = $(modal);
 	var $dialog = $modal.find('.modal_dialog');
 	var $backdrop = $modal.find('.modal_backdrop');
 	var topGap = 0;
-
 	if (posi === 'bottom'){
 		topGap = $modal.outerHeight() - $dialog.outerHeight();		
-	} else if (posi === 'middle'){
-		topGap = ($modal.outerHeight() / 2) - ($dialog.outerHeight() / 2);
+	} else if (posi === 'middle') {
+		topGap = ($modal.outerHeight() / 2 ) - ($dialog.outerHeight() / 2);
 	} else {
 		topGap = 0;
 	};
-
 	topGap >= 0 ? $dialog.css('top', topGap) : $dialog.css('top', 'auto');
-
 	var winH = $(window).height();
 	var dialogH = $dialog.outerHeight();
-
 	dialogH > winH ? $backdrop.css('height', dialogH) : $backdrop.css('height', winH);
 };
 
 /* 팝업 닫기 */
 var modalClose = function(modal){
 	var $modal = $(modal);
-
 	$('body').removeClass('modal_show');
-
 	$modal.removeClass('on').fadeOut(300, function(){
-		$(this).find('.modal_backdrop').css('height', '').next('.modal_dialog').css('top', '');
+		$(this).find('.modal_backdrop').css('height', '').next('.modal_dialog').css('top','');
 	});
 };
--- pc/js/common.js
+++ pc/js/common.js
@@ -25,7 +25,19 @@ $(document).ready(function(){
 		onResize('.snb_area');
   });
 
+	/* input text focus */
+	var inputTextFocus = function(inp){
+		var $inp = $(inp).find('input');
+		$inp.focus(function(){
+			$(this).parent(inp).addClass('inp_focus');
+		});
+		$inp.blur(function(){
+			$(this).parent(inp).removeClass('inp_focus');
+		});
+	};
+	inputTextFocus('.inp_area');
 	/* snb */
+
 	$('.snb_area .btn_toggle').click(function(){
 		$('.snb_area').removeClass('m_defult').toggleClass('m_type2');
 	});
@@ -40,6 +52,8 @@ $(document).ready(function(){
 		preventDefault();
 	});
 
+	/* selectbox */
+	$('select:not(.ignore)').niceSelect();
 
 	/* 기능 사용 여부 토글 */
 	var toggleBtn = function(btn){
@@ -80,3 +94,55 @@ $(document).ready(function(){
 		$('.tab_cont>div').eq(tabView).show();
 	});
 });
+
+/* 팝업 열기 */
+var modalOpen = function(modal, posi){
+	var $modal = $(modal);
+	$('body').addClass('modal_show');
+	$modal.fadeIn(100, function(){
+		$(this).addClass('on');
+	});
+	modalPosi(modal, posi);
+	$(window).bind('orientationchange resize', function(){
+		if ($modal.hasClass('on')){
+			modalPosi(modal, posi);
+		};
+	});
+	/* $modal.find('.modal_backdrop').click(function(){
+		modalClose(modal);
+	}); */
+	$modal.find('.btn_close').click(function(){
+		modalClose(modal);
+	});
+	$modal.find('.btn_gray').click(function(){
+		modalClose(modal);
+	});
+};
+
+/* 팝업 위치 */
+var modalPosi = function(modal, posi) {
+	var $modal = $(modal);
+	var $dialog = $modal.find('.modal_dialog');
+	var $backdrop = $modal.find('.modal_backdrop');
+	var topGap = 0;
+	if (posi === 'bottom'){
+		topGap = $modal.outerHeight() - $dialog.outerHeight();		
+	} else if (posi === 'middle') {
+		topGap = ($modal.outerHeight() / 2 ) - ($dialog.outerHeight() / 2);
+	} else {
+		topGap = 0;
+	};
+	topGap >= 0 ? $dialog.css('top', topGap) : $dialog.css('top', 'auto');
+	var winH = $(window).height();
+	var dialogH = $dialog.outerHeight();
+	dialogH > winH ? $backdrop.css('height', dialogH) : $backdrop.css('height', winH);
+};
+
+/* 팝업 닫기 */
+var modalClose = function(modal){
+	var $modal = $(modal);
+	$('body').removeClass('modal_show');
+	$modal.removeClass('on').fadeOut(300, function(){
+		$(this).find('.modal_backdrop').css('height', '').next('.modal_dialog').css('top','');
+	});
+};
