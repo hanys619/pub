fc2a7d9fb362b0bdefbf8f95273a272d1da1e0f4
hanys619 <song619@gmail.com>
2023-06-22 09:57:16 +0900

    125% 배율 디바이스 소수점화면사이즈 대응

--- pc/js/common.js
+++ pc/js/common.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
 	};
 	var onResize = function(target) {
 		$(document).ready(function(){
-			$(target).css('height', $(window).height() - 1 + 'px'); //125% 배율 디바이스 소수점 대응(-1px)
+			$(target).css('height', $(window).height());
+			if ($(target).hasClass('main') || $(target).hasClass('hold')){
+				$(target).css('height', $(window).height() - 1 + 'px'); //125% 배율 디바이스 소수점 대응(-1px)
+			};
 		});
 	}
 	onResize('.wrap');
@@ -22,7 +25,7 @@ $(document).ready(function(){
 		};
 		var onResize = function(target) {
 			$(document).ready(function(){
-				$(target).css('height', $(window).height() - 1 + 'px'); //125% 배율 디바이스 소수점 대응(-1px)
+				$(target).css('height', $(window).height()); //125% 배율 디바이스 소수점 대응(-1px)
 			});
 		}
 		onResize('.wrap');
